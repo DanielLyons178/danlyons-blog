72f12214a9313c5d8eb14647ac573dcf112d60b3
Dan Lyons <daniel.lyons178@live.com>
2021-02-24 20:08:04 +0000

    Color

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -43,7 +43,7 @@ export default function Navigation() {
               padding: 0;
               position: fixed;
               top: 0;
-              // background-color: #fff;
+              background-color: #fff9ec;
               display: flex;
               flex-direction: column;
               justify-content: center;
